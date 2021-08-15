d2048ce78f46d71dfeb95392985e9e44b1a5338d
MrBamboo <nicolas@bamboosoft.ca>
2021-08-14 23:49:35 -0400

    feat: Make text move in screen

--- flappy/src/flappy.c
+++ flappy/src/flappy.c
@@ -2,6 +2,22 @@
 
 extern char snesfont;
 
+void put16ColorsMode()
+{
+	setMode(BG_MODE1, 0);
+	bgSetDisable(1);
+	bgSetDisable(2);
+}
+
+void clearConsole()
+{
+	unsigned short i;
+	for (i = 0; i < 26; i++)
+	{
+		consoleDrawText(0, i, "                                ");
+	}
+}
+
 int main(void)
 {
 	// Initialize SNES
@@ -11,18 +27,24 @@ int main(void)
 	consoleInitText(0, 0, &snesfont);
 
 	// Now Put in 16 color mode and disable Bgs except current
-	setMode(BG_MODE1, 0);
-	bgSetDisable(1);
-	bgSetDisable(2);
+	put16ColorsMode();
 
-	// Draw a wonderfull text :P
-	consoleDrawText(6, 14, "! Test test test !");
+	unsigned short yText = 0;
 
 	// Wait for nothing :P
 	setScreenOn();
 
 	while (1)
 	{
+		clearConsole();
+
+		yText++;
+		consoleDrawText(0, yText, "test");
+		if (yText > 24)
+		{
+			yText = 0;
+		}
+
 		WaitForVBlank();
 	}
 	return 0;

068eb359d33d58381ee95f54bb91a6c18f5d69e8
MrBamboo <nicolas@bamboosoft.ca>
2021-08-14 22:35:10 -0400

    refactor: Try to change base display text

--- flappy/src/flappy.c
+++ flappy/src/flappy.c
@@ -16,9 +16,7 @@ int main(void)
 	bgSetDisable(2);
 
 	// Draw a wonderfull text :P
-	consoleDrawText(10, 10, "Hello World !");
-	consoleDrawText(6, 14, "WELCOME TO PVSNESLIB");
-	consoleDrawText(3, 18, "HTTPS://WWW.PORTABLEDEV.COM");
+	consoleDrawText(6, 14, "! Test test test !");
 
 	// Wait for nothing :P
 	setScreenOn();
